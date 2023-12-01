94c10d30c41b311f37e649707778c285e869de97
Ala <atrash.ala@gmail.com>
2023-12-01 19:15:41 +0100

    adding the services plans

--- src/plan/plan.service.ts
+++ src/plan/plan.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PlanService {}
+export class PlanService {
+    
+    getServices= () => {
+        return ['service1', 'service2']
+    }
+}
